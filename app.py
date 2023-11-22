81a2bfd58e95f6fae7b0ac34eb3230b1a3c62f6f
TechTrapture <143475073+vishal-bulbule@users.noreply.github.com>
2023-11-22 21:38:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def fetch_cricket_scores():
 
     headers = {
         "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com",
-        "X-RapidAPI-Key": "1bd0a14833mshc18ed4be5953504p1236e8jsn709d3a0bc623"  # Replace with your RapidAPI key
+        "X-RapidAPI-Key": "Replace with your RapidAPI key"  
     }
     response = requests.get(url, headers=headers)
     data = response.json()
